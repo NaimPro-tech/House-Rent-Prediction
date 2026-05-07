177031fb2c93d3037e7d4077622d547749fd2041
Naim Prottay <naimbinmasud@gmail.com>
2026-05-07 23:09:20 +0600

    Added CSS code to upgrade UI

--- app.py
+++ app.py
@@ -34,18 +34,51 @@ def predict_ui():
         for col in float_cols:
             if col in input_data:
                 input_data[col] = float(input_data[col])
+        
+        #add input validation
+        required_fields = [
+            "area_locality",
+            "bhk",
+            "size",
+            "bathroom",
+            "living_floor",
+            "total_floor"
+        ]
+        for field in required_fields:
+            if field not in input_data or input_data[field]=="":
+                return render_template(
+                    "index.html",
+                    prediction_text = f"{field} is required!"
+                )
+        
+        #numeric validation
+        if input_data["bhk"]<=0:
+            return render_template(
+                "index.html",
+                prediction_text = "BHK must be greater than Zero"
+            )
 
+        if input_data["size"]<=0:
+            return render_template(
+                "index.html",
+                prediction_text = "How can you live without space in your house!!!"
+            )
+        
+        if input_data["bathroom"]<=0:
+            return render_template(
+                "index.html",
+                prediction_text = "Your home should have Bathroom."
+            )
+
+        if input_data["living_floor"]>input_data["total_floor"]:
+            return render_template(
+                "index.html",
+                prediction_text = "Your are too above from your building."
+            )
+        
         df = pd.DataFrame([input_data])
         df = create_features(df=df)
 
-        # #encoding
-        # df['area_locality'] = encoder.transform(df['area_locality'])
-
-        # #one hot (same as training)
-        # df = pd.get_dummies(df)
-
-        # #match columns
-        # df = df.reindex(columns=features, fill_value=0)
 
         df = preprocess(df=df, encoder=encoder, features=features)
 
@@ -61,7 +94,7 @@ def predict_ui():
     except Exception as e:
         return render_template(
             "index.html",
-            prediction_text = f"Error: {str(e)}"
+            prediction_text = f"Something went wrong!: {str(e)}"
         )
 
 
